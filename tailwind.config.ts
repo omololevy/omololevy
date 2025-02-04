a96bf492256359413f7d602f0184b718a62e2c36
omololevy <cotechlevy@gmail.com>
2025-02-04 04:44:14 +0300

    configure tailwind with global css for styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class', // Add this line
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          yellow: "#ffbd59",
+          blue: "#05347e",
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
