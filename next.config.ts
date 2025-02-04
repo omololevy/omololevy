309220f3eb1914a1162cb2addc4634a58542cec2
omololevy <cotechlevy@gmail.com>
2025-02-04 18:43:36 +0300

    revert the config for images

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['images.unsplash.com'],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
