11ee04b177da32a7ed6b6704caf51f073cfa7977
omololevy <cotechlevy@gmail.com>
2025-02-04 07:33:17 +0300

    adjust the layout to capture some of the previous settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "./context/ThemeContext";
 
@@ -16,6 +17,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Kiungor - Embracing New Beginnings, Together!",
   description: "Connect with other migrants who share your interests and experiences, gain access to migration resources, job listings, and language translation services.",
+  keywords: "immigrants, immigration, travel abroad, immigrants community, like-minded, migrants, migration, migrants community, Kiungor, jobs for immigrants, international students, study abroad, work abroad",
+  authors: [{ name: "Kiungor" }],
+  other: {
+    "google-site-verification": "YOUR_VERIFICATION_CODE",
+  },
 };
 
 export default function RootLayout({
@@ -25,10 +31,51 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Preconnect to Google Fonts */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap" 
+          rel="stylesheet"
+          as="style"
+        />
+        <link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider>
           {children}
         </ThemeProvider>
+
+        {/* Segment Analytics */}
+        <Script id="segment-analytics" strategy="afterInteractive">
+          {`
+            !function(){var i="analytics",analytics=window[i]=window[i]||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","screen","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware","register"];analytics.factory=function(e){return function(){if(window[i].initialized)return window[i][e].apply(window[i],arguments);var n=Array.prototype.slice.call(arguments);if(["track","screen","alias","group","page","identify"].indexOf(e)>-1){var c=document.querySelector("link[rel='canonical']");n.push({__t:"bpc",c:c&&c.getAttribute("href")||void 0,p:location.pathname,u:location.href,s:location.search,t:document.title,r:document.referrer})}n.unshift(e);analytics.push(n);return analytics}};for(var n=0;n<analytics.methods.length;n++){var key=analytics.methods[n];analytics[key]=analytics.factory(key)}analytics.load=function(key,n){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.setAttribute("data-global-segment-analytics-key",i);t.src="https://cdn.segment.com/analytics.js/v1/"+key+"/analytics.min.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r);analytics._loadOptions=n};analytics._writeKey="CUl1S9CtQ67b45do0Nhc9ytKfh7XOdYw";;analytics.SNIPPET_VERSION="5.2.0";
+            analytics.load("CUl1S9CtQ67b45do0Nhc9ytKfh7XOdYw");
+            analytics.page();
+          }}();
+          `}
+        </Script>
+
+        {/* Google Analytics */}
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-GCZKBF5524" 
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GCZKBF5524');
+          `}
+        </Script>
+
+        {/* Zendesk Widget */}
+        <Script
+          id="ze-snippet"
+          src="https://static.zdassets.com/ekr/snippet.js?key=86ba26ac-d8ac-4eae-8530-16fc6ef87316"
+          strategy="lazyOnload"
+        />
       </body>
     </html>
   );
